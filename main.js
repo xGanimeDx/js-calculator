9fc597c9441cbe3155d672b119b19231e5fe6e7c
Stanislav Radevich <stanislav.radevich@zenitech.co.uk>
2022-11-02 23:08:43 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let runningTotal = 0;
+let buffer = "";
+let previousOperator;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
